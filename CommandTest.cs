722b0a6bcfbaef57db430ac8e65c956eff99eb8d
fr34kyn01535 <fr34kyn01535@bam.yt>
2014-12-31 22:47:25 +0100

    Initial comitt

---
+++ CommandTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+using SDG;
+using UnityEngine;
+using Rocket.RocketAPI;
+
+namespace ExamplePlugin
+{
+    class CommandTest : RocketCommand
+    {
+        public void Execute(SDG.SteamPlayerID caller, string command)
+        {
+            ChatManager.say(String.Format("Your testmessage is: {0}", ExamplePlugin.Configuration.aText));
+        }
+
+        public string Name
+        {
+            get { return "Test"; }
+        }
+
+        public string Help
+        {
+            get { return "This is a helpmessage"; }
+        }
+    }
+}
---
+++ ExampleConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Rocket.RocketAPI;
+
+namespace ExamplePlugin
+{
+    public class ExampleConfiguration : RocketConfiguration
+    {
+        public string aText = "test";
+        public int aNumber = 42;
+    }
+}
